dec020d6fa08c33c0de734920ee03f6f9c075fb1
edwardlim01 <edward.lim@protiviti.com>
2025-08-22 14:42:24 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 18:55:04 2024
+
+@author: minwuu01
+"""
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
