93b9bd4b69b817e97cf154e983ee0a982321e46f
edwardlim01 <edward.lim@protiviti.com>
2025-08-22 15:55:14 -0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 21:27:49 2024
+
+@author: minwuu01
+"""
+
+from flask import Flask
+from .routes import register_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+
+    register_routes(app)
+
+    return app
+
+
