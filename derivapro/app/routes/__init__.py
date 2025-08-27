514c5438ac0e5b128dc9a80cb83e1c95c52116c6
edwardlim01 <edward.lim@protiviti.com>
2025-08-27 17:32:27 -0400

    Update __init__.py

--- derivapro/app/routes/__init__.py
+++ derivapro/app/routes/__init__.py
@@ -1,4 +1,5 @@
 def register_routes(app):
+    # Relative imports for files in the same folder
     from .index import index_bp
     from .vanilla_options import vanilla_options_bp
     from .exotic_options import exotic_options_bp
@@ -9,10 +10,13 @@ def register_routes(app):
     from .credit_derivatives import credit_derivatives_bp
     from .equity_derivatives import equity_derivatives_bp
     from .bonds import nc_bonds_bp
-    from app.routes.volatility_surface import volatility_surface_bp
-    from app.routes.prepayment import prepayment_bp
     from .term_structure import term_structure_bp
 
+    # Absolute imports for files outside this folder
+    from derivapro.app.routes.volatility_surface import volatility_surface_bp
+    from derivapro.app.routes.prepayment import prepayment_bp
+
+    # Register blueprints
     app.register_blueprint(index_bp, url_prefix='/')
     app.register_blueprint(vanilla_options_bp, url_prefix='/vanilla-options')
     app.register_blueprint(exotic_options_bp, url_prefix='/exotic-options')
@@ -23,7 +27,6 @@ def register_routes(app):
     app.register_blueprint(credit_derivatives_bp, url_prefix='/credit-derivatives')
     app.register_blueprint(equity_derivatives_bp, url_prefix='/equity-derivatives')
     app.register_blueprint(nc_bonds_bp, url_prefix='/noncallable-bonds')
-    # Register the volatility surface blueprint
     app.register_blueprint(volatility_surface_bp, url_prefix="/volatility_surface")
     app.register_blueprint(prepayment_bp, url_prefix="/prepayment")
     app.register_blueprint(term_structure_bp, url_prefix="/term-structure")
