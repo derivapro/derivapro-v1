30a974588a33b2af36dbc08e89ada2499f3b3f61
edwardlim01 <edward.lim@protiviti.com>
2025-08-27 17:42:36 -0400

    Update __init__.py

--- derivapro/app/__init__.py
+++ derivapro/app/__init__.py
@@ -10,11 +10,12 @@ from derivapro.app.routes import register_routes
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')
+    app.config.from_object('derivapro.app.config.Config')
 
     register_routes(app)
 
     return app
+
 
 
 
