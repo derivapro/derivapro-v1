59af616f329af5f852c3a9f766e6f81cf1ed361d
edwardlim01 <edward.lim@protiviti.com>
2025-08-28 11:04:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="derivapro",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
@@ -18,4 +18,5 @@ setup(
         ],
     },
 )
+
 
