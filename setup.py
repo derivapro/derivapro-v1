c33467dd061c54d883e3105b9e8bdab67290cbff
edwardlim01 <edward.lim@protiviti.com>
2025-08-28 00:28:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+# Read requirements
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
@@ -8,7 +9,8 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=requirements,
+    include_package_data=True,  # ensures package_data and MANIFEST.in are used
+    package_data={
+        "derivapro": ["*.md"],  # include all Markdown files in the package
+    },
 )
-
-
-
